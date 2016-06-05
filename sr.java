eeb47aa67b8d0be71ad29b55f71a79e31619ddea
sivaa31 <sivabagyam2995@gmail.com>
2016-06-05 15:57:47 +0530

    Create sr.java

---
+++ sr.java
@@ -0,0 +1,9 @@
+class st
+{
+public static void main(String[] agrs)
+{
+Scanner sc=new Scanner(System.in);
+String n=sc.next();
+int num= Integer.parseInt(numberAsString);
+System.out.println("The number is: " + num);
+};
