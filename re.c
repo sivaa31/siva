33377a85be53e543198835dc9cf94dc559c9eb8d
sivaa31 <sivabagyam2995@gmail.com>
2016-06-09 16:28:13 +0530

    Create re.c

---
+++ re.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void) {
+	int n,hash[100]={0};
+	scanf("%d",&n);
+	int a[n],i;
+	for(i=0;i<n;i++)
+	{
+	scanf("%d",&a[i]);
+	hash[a[i]]++;
+	}
+for(i=0;i<n;i++)	
+{
+	if(hash[i]>1)
+	printf("%d",a[i]);
+}
+
+	
+	
+	return 0;
+}
