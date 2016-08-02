167d94d23239b9da782cd228b5a75a396e1069f4
sivaa31 <sivabagyam2995@gmail.com>
2016-08-02 22:26:26 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d",&n);
+	if(n>0)
+	printf("positive\n");
+	else if(n<0)
+	printf("negative\n");
+	else
+	printf("Zero");
+	// your code goes here
+	return 0;
+}
